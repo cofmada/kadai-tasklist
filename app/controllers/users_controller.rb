e89d04354359ca6d06f491b5e1ee555d65e248c3
cof.iland <shima4113@gmail.com>
2021-09-23 05:19:59 +0000

    add new action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザー登録が完了しました！'
+      redirect_to @user
+    else
+      flash.now[:danger] = '登録できませんでした。もう一度入力してください。'
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   
   root to: 'tasks#index'
   
+  get 'ユーザー登録', to: 'users#new'
+  resources :users, only:[ :show, :create, :edit, :destroy]
   resources :tasks
 end
